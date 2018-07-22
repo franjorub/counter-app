a18cdae55621f27a5bef3e5ddfedb88751161e89
Francisco Rubial <franjorub@gmail.com>
2018-07-22 10:08:41 -0400

    uso del operador && para devolver cosas

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -2,27 +2,22 @@ import React, { Component } from "react";
 
 class Counter extends Component {
   state = {
-    count: 0
+    count: 0,
+    tags: []
   };
 
-  styles = {
-    fontSize: "10px",
-    fontWeight: "bold"
-  };
+  renderTags() {
+    if (this.state.tags.length === 0) return <p>There are no tags!</p>;
+    return <ul>{this.state.tags.map(tag => <li key={tag}>{tag}</li>)}</ul>;
+  }
 
   render() {
     return (
       <div>
-        <span className="badge badge-primary m-2">{this.formatCount()}</span>
-        <button className="btn btn-secondary btn-sm">Increment</button>
+        {this.state.tags.length === 0 && "Please create a new tag"}
+        {this.renderTags()}
       </div>
     );
   }
-
-  formatCount() {
-    const { count } = this.state;
-    return count === 0 ? <h1>Zero</h1> : count;
-  }
 }
-
 export default Counter;
