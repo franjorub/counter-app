5c6f9c97a90149790af91075affeb88ea91b8233
Francisco Rubial <franjorub@gmail.com>
2018-07-22 02:06:25 -0400

    Añadido la etiqueta para el total y un boton de incremento

--- src/components/counter.jsx
+++ src/components/counter.jsx
@@ -1,13 +1,27 @@
 import React, { Component } from "react";
 
 class Counter extends Component {
+  state = {
+    count: 0
+  };
+
+  styles = {
+    fontSize: "10px",
+    fontWeight: "bold"
+  };
+
   render() {
     return (
       <div>
-        <h1>Hellow World</h1>
-        <button>Increment</button>
+        <span className="badge badge-primary m-2">{this.formatCount()}</span>
+        <button className="btn btn-secondary btn-sm">Increment</button>
       </div>
     );
+  }
+
+  formatCount() {
+    const { count } = this.state;
+    return count === 0 ? <h1>Zero</h1> : count;
   }
 }
 
